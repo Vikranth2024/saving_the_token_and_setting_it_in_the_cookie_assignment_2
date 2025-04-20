849871657dd8e5e9576234e136d9fa26ce9a169e
Vikranth2024 <vikranthsabari436@gmail.com>
2025-04-20 22:50:55 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const jwt = require('jsonwebtoken');
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+
+  return jwt.sign(payload,secret, {expiresIn:'7d'});
+
 };
 
-module.exports = encrypt;
+const verifyToken = (token, secret) => {
+  try {
+    const decoded = jwt.verify(token, secret);
+    console.log("Token is valid:", decoded);
+    return decoded;
+
+  } catch (err) {
+    console.error('Token is invalid or expired:', err.message);
+    return null;
+  }
+};
+
+module.exports = { encrypt, verifyToken };
